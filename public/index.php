b7d8c101932d810213fec6a8672014a1f7cb80c8
AgustinMadygraf <agustin.mtto.madygraf>
2025-01-05 19:13:14 -0300

    Enable error reporting for improved debugging in index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <!--
 Path: app/index.php
-
 -->
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../app/helpers/debug_helper.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/controllers/URLController.php';
